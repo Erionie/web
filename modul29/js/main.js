7ce21b4a459286aae03a08dc2043ada6dfe950a6
Erionie <erionnklinaku@gmail.com>
2026-06-25 17:35:46 +0200

    commit

---
+++ modul29/js/main.js
@@ -0,0 +1,31 @@
+var pl=['html','css','javascript','php','wordpres','react'];
+
+console.log(pl[2]);
+console.log(pl[4]);
+
+pl.push('java');
+console.log(pl);
+
+pl.pop();
+console.log(pl);
+
+pl.unshift('pyathon');
+console.log(pl);
+
+pl.shift();
+console.log(pl);
+
+console.log(Math.random()*200);
+console.log(Math.floor(Math.random()*200));
+
+var studentat=['erioni','ensari','diari'];
+
+var[s1,s2,s3]=studentat;
+
+console.log(s2);
+
+var places=("barcelona","obiliq","fushekosove","paris","mitrovic","stamboll","prishtina");
+var[,,firstplace,secondplace]=places;
+console.log(secondplace);
+
+
